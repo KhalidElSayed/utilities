26a103c98939acddc77daf67611c10929473dd1f
Bernat Borrás <bernatbor15@gmail.com>
2013-05-24 23:06:45 +0200

    BaseCUrsor with contentValues[]

--- bbdd/BaseCursor.java
+++ bbdd/BaseCursor.java
@@ -1,7 +1,10 @@
-package com.alorma.bbdd.cursorhelpers;
+package com.alorma.colibri.bbdd.cursor;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import com.alorma.colibri.bean.item.Party;
+
+import java.util.List;
 
 /**
  * Created by alorma on 19/05/13.
@@ -9,6 +12,19 @@ import android.database.Cursor;
 public abstract class BaseCursor<K> {
 
     public abstract ContentValues write(K k);
+
     public abstract K read(Cursor cursor);
+
+    public ContentValues[] write(List<K> ks) {
+
+        ContentValues[] values = new ContentValues[ks.size()];
+
+        int i = 0;
+        for (K k : ks) {
+            values[i++] = write(k);
+        }
+
+        return values;
+    }
 
 }
